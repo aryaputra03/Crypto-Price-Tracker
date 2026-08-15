f2a466d7a6b57b72dd853e93b73971a9a26837d1
Ganendra Aryaputra <kemaproject01@gmail.com>
2026-08-16 05:06:11 +0700

    netlify final 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
+
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    tanstackStart({
-      server: {
-        preset: 'vercel', // Ini "mantra" ajaibnya agar Vercel mengerti!
-      },
-    }),
-    viteReact(),
-  ],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
 })
+
+export default config
